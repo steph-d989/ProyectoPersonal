67e42effa9f41d251431a5e451099762412878ee
steph-d989 <steb_d@hotmail.com>
2024-07-17 21:57:37 +0200

    navBar

--- client/src/components/Header/Nav/Nav.jsx
+++ client/src/components/Header/Nav/Nav.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../../assets/Logo.jpeg';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={logo} alt="Logo de Mi Empresa de Juegos" className="logo-image" />
+        </Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/catalogo">Inicio</Link></li>
+        <li><Link to="/contacto">Contacto</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
