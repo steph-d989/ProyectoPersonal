c3f74891e8f2e03d1b9261489be4cf72611bbf9a
steph-d989 <steb_d@hotmail.com>
2024-07-20 19:03:24 +0200

    scss falta usuario

--- client/src/components/Header/Nav/Nav.jsx
+++ client/src/components/Header/Nav/Nav.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from '../../../../public/Frame_9.svg';
 
+
 const Nav = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -10,6 +17,25 @@ const Nav = () => {
           <img src={logo} alt="Logo de Mi Empresa de Juegos" className="logo-image" />
         </Link>
       </div>
+
+      {/* Icono de menú para móviles */}
+      <div className="menu-toggle" onClick={toggleMobileMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* Menú móvil */}
+      <div className={`mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+        <ul>
+          <li><Link to="/catalogo">Inicio</Link></li>
+          <li><Link to="/usuario/steph_d@hotmail.com">Mi perfil</Link></li>
+          <li><Link to="/quienes_somos">Quienes somos?</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </div>
+
+      {/* Menú completo para pantallas medianas y grandes */}
       <ul className="navbar-links">
         <li><Link to="/catalogo">Inicio</Link></li>
         <li><Link to="/usuario/steph_d@hotmail.com">Mi perfil</Link></li>
